39599b258201afa5ed763a2cb2c0dae025703290
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-09-12 15:34:53 -0400

    Update 1_Home.py

--- 1_Home.py
+++ 1_Home.py
@@ -19,7 +19,7 @@ def ask_bot(input_text):
                 huggingfacehub_api_token="HuggingFace_API_KEY", # Replace with your own API key or use ours: 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
                 task="text-generation",
                 model_kwargs = {
-                    "max_new_tokens":200 # define the maximum number of tokens the model may produce in its answer         
+                    "max_new_tokens":1024 # define the maximum number of tokens the model may produce in its answer         
                 }
             )
     # LLMPredictor: to generate the text response (Completion)
