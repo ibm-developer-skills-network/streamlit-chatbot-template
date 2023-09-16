f30d280c1b0acb17b87c10d577480d94fd6ce24b
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-09-16 19:52:33 -0400

    Update 1_Home.py

--- 1_Home.py
+++ 1_Home.py
@@ -16,13 +16,11 @@ from ibm_watson_machine_learning.foundation_models import Model
 DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 # Global variables
-conversation_retrieval_chain = None
-chat_history = []
 llm_hub = None
 embeddings = None
 
-Watsonx_API = "uvnQIfnjPk2Jpszy0hAvr80xCUAudclZsltCi3gYxAVu"
-Project_id= "177ab670-c7d0-4f34-894f-228297d644d9"
+Watsonx_API = "Watsonx_API"
+Project_id= "Project_id"
 
 # Function to initialize the Watsonx language model and its embeddings used to represent text data in a form (vectors) that machines can understand. 
 def init_llm():
@@ -43,13 +41,13 @@ def init_llm():
     }
 
     
-    LLAMA2_model = Model(
+    model = Model(
         model_id= 'meta-llama/llama-2-70b-chat',
         credentials=credentials,
         params=params,
         project_id=Project_id)
 
-    llm_hub = WatsonxLLM(model=LLAMA2_model)
+    llm_hub = WatsonxLLM(model=model)
 
     #Initialize embeddings using a pre-trained model to represent the text data.
     embeddings = HuggingFaceInstructEmbeddings(
@@ -58,58 +56,34 @@ def init_llm():
 
 init_llm()
 
-# Store the conversation history in a List
-conversation_history = []
-
 # load the file
 documents = SimpleDirectoryReader(input_files=["data.txt"]).load_data()
 
+# LLMPredictor: to generate the text response (Completion)
+llm_predictor = LLMPredictor(
+        llm=llm_hub
+)
+                                 
+# Hugging Face models can be supported by using LangchainEmbedding to convert text to embedding vector	
+embed_model = LangchainEmbedding(embeddings)
+#embed_model = LangchainEmbedding(HuggingFaceEmbeddings())
+
+# ServiceContext: to encapsulate the resources used to create indexes and run queries    
+service_context = ServiceContext.from_defaults(
+        llm_predictor=llm_predictor, 
+        embed_model=embed_model
+)      
+# build index
+index = GPTVectorStoreIndex.from_documents(documents, service_context=service_context)
+
 def ask_bot(input_text):
 
-    global documents
-
-    # LLMPredictor: to generate the text response (Completion)
-    llm_predictor = LLMPredictor(
-            llm=llm_hub
-    )
-                                     
-    # Hugging Face models can be supported by using LangchainEmbedding to convert text to embedding vector	
-    embed_model = LangchainEmbedding(embeddings)
-    
-    # ServiceContext: to encapsulate the resources used to create indexes and run queries    
-    service_context = ServiceContext.from_defaults(
-            llm_predictor=llm_predictor, 
-            embed_model=embed_model
-    )      
-    # build index
-    index = GPTVectorStoreIndex.from_documents(documents, service_context=service_context)
-
-    PROMPT_QUESTION = """
-    Your name is IBM Skills Network. Briefly introduce yourself first if it's the first time answering a question.
-    Your conversation with the human is recorded in the chat history below. After the self-introduction, you don't need to repeat mentioning your name or introducing yourself actively. If the recruiter asks about the skills or experiences you have with url links, answer it with the link.
-    
-    History:
-    "{history}"
-    
-    Now continue the conversation with the human. If you do not know the answer, politely ask for more information.
-    Human: {input}
-    Assistant:"""
-    
-    # This will wrap the default prompts that are internal to llama-index
-    #query_wrapper_prompt = SimpleInputPrompt(f"{PROMPT_QUESTION}<|USER|>{input_text}<|ASSISTANT|>")
-    
-    # update conversation history
-    global conversation_history
-    history_string = "\n".join(conversation_history)
-    print(f"history_string: {history_string}")  
-    
-    # query LlamaIndex and llama-2-70b-chat for the AI's response
+    global index
+ 
+    # query LlamaIndex and GPT-3.5 for the AI's response
     output = index.as_query_engine().query(input_text)
-    print(f"output: {output}")   
+    print(f"output: {output}")
     
-    # update conversation history with user input and AI's response
-    conversation_history.append(input_text)
-    conversation_history.append(output.response)
     return output
 
 # get the user's input by calling the get_text function
