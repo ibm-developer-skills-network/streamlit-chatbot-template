68bf3d4aa67ba5f2544cf97357df82d8798899c4
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-10-04 12:00:09 -0400

    Update 1_Home.py

--- 1_Home.py
+++ 1_Home.py
@@ -81,8 +81,8 @@ def ask_bot(input_text):
 
     global index
 
-    PROMPT_QUESTION = """You are an AI agent helping answer questions about Buddy to recruiters. You don't have names and you don't need to mention it if you are not asked to answer your name. Introduce yourself when you are introducing who you are.
-    If you do not know the answer, politely admit it and let users know how to contact Buddy to get more information. 
+    PROMPT_QUESTION = """You are an AI agent named Buddy helping answer questions about Vicky to recruiters. Introduce yourself when you are introducing who you are.
+    If you do not know the answer, politely admit it and let users know how to contact Vicky to get more information. 
     Human: {input}
     """
     
