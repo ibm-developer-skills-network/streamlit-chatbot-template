213eec7d2968f1a0d30174af921bb5007004c41a
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-10-11 10:48:20 -0400

    Update and rename constant.py to utils/constants.py

---
+++ utils/constants.py
@@ -0,0 +1,53 @@
+chat = {
+    "TITLE1": "About Me",
+    "TITLE2": "AI Chat",
+    "menu_v": {
+   "container": {"background-color": "#a7c5ff"},
+   "icon": {"color": "white", "font-size": "25px"}, 
+   "nav-link": {"font-size": "25px", "text-align": "center", "color": "white", "margin":"0px", "--hover-color": "#1b97ff"},
+   "nav-link-selected": {"background-color": "#1b97ff"}
+    },
+    "menu_h": {
+        "container": {"padding": "0px",
+                      "display": "grid",
+                      "margin": "0!important",
+                      "background-color": "#212121"
+                      },
+        "icon": {"color": "#bd93f9", "font-size": "14px"},
+        "nav-link": {
+            "font-size": "14px",
+            "text-align": "center",
+            "margin": "auto",
+            "background-color": "#262626",
+            "height": "30px",
+            "width": "13rem",
+            "color": "#edff85",
+            "border-radius": "5px"
+        },
+        "nav-link-selected": {
+            "background-color": "#212121",
+            "font-weight": "300",
+            "color": "#f7f8f2",
+            "border": "1px solid #e55381"
+        }
+    }
+}
+
+info = {
+   "About":"Hello, I'm Vicky, and I'm passionate about leveraging data and technology to drive meaningful insights and solutions in business. You can explore my portfolio of free courses and resources at cognitiveclass.ai to see how I can contribute to your team's success.",
+   "Project":"https://author.skills.network/instructors/vicky_kuo",
+   "Medium":"https://medium.com/@vicky-note/about",
+   "City":"Toronto, Canada",
+   "Resume": "https://drive.google.com/file/d/1e_trgOgzwYIY6eMSvjmyjgqBcCOdueZ1/view?usp=sharing"
+}
+
+embed_rss= {
+    'rss':"""<div style="overflow-y: scroll; height:500px; background-color:white;"> <div id="retainable-rss-embed" 
+        data-rss="https://medium.com/feed/@vicky-note"
+        data-maxcols="3" 
+        data-layout="grid"
+        data-poststyle="inline" 
+        data-readmore="Read the rest" 
+        data-buttonclass="btn btn-primary" 
+        data-offset="0"></div></div> <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>"""
+}
