72c58fb35ebc296a0fddf85d55e93d801f010143
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-10-11 21:16:48 -0400

    Update 3_📄Resume.py

--- pages/3_📄Resume.py
+++ pages/3_📄Resume.py
@@ -12,7 +12,6 @@ local_css("styles/styles_main.css")
     
 # get the variables from constants.py
 pronoun = info['Pronoun']
-full_name = info['Full_Name']
 
 # app sidebar
 with st.sidebar:
@@ -33,7 +32,7 @@ with st.sidebar:
             """
         )
         
-    st.caption(f"© Made by {full_name} 2023. All rights reserved.")
+    st.caption(f"© Made by Vicky Kuo 2023. All rights reserved.")
  
 st.title("📝 Resume")
 
