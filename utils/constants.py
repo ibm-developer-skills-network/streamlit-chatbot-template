7a1b3f3c67fd80e0f2e91a27fc6561d3542dac4a
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-10-11 20:35:23 -0400

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -34,6 +34,8 @@ chat = {
 }
 
 info = {
+   "Name": "Vicky", 
+   "Full_Name":"Vicky Kuo", 
    "About":"Hello, I'm Vicky, and I'm passionate about leveraging data and technology to drive meaningful insights and solutions in business. You can explore my portfolio of free courses and resources at cognitiveclass.ai to see how I can contribute to your team's success.",
    "Project":"https://author.skills.network/instructors/vicky_kuo",
    "Medium":"https://medium.com/@vicky-note/about",
