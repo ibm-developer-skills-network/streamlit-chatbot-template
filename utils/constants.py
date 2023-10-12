1e47fffdfb2652b84fa9179166ddab6ac0274bfe
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-10-12 01:41:13 -0400

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -46,7 +46,7 @@ info = {
    "Email": "vicky.kuo.contact@gmail.com" 
 }
 
-Projects = [
+projects = [
         {
             "title": "Unlocking Multilingual Magic: Babel Fish with LLM STT TTS",
             "description": "Text-to-Speech, Speech-To-Text, LLM, watsonx",
@@ -102,6 +102,13 @@ Projects = [
             "link": "https://cognitiveclass.ai/courses/course-v1:IBMSkillsNetwork+GPXX0FTNEN+v1"
         }                  
     ]
+
+endorsements = {
+    "img1": "https://user-images.githubusercontent.com/90204593/238169843-12872392-f2f1-40a6-a353-c06a2fa602c5.png",
+    "img2": "https://user-images.githubusercontent.com/90204593/238171251-5f4c5597-84d4-4b4b-803c-afe74e739070.png",
+    "img3": "https://user-images.githubusercontent.com/90204593/238171242-53f7ceb3-1a71-4726-a7f5-67721419fef8.png"
+}
+
 embed_rss= {
     'rss':"""<div style="overflow-y: scroll; height:500px; background-color:white;"> <div id="retainable-rss-embed" 
         data-rss="https://medium.com/feed/@vicky-note"
