3cae6372c0705f761ba5d7789e45efec9efb58c0
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-10-12 00:04:46 -0400

    Update 2_💬AI Assistant Chat.py

--- pages/2_💬AI Assistant Chat.py
+++ pages/2_💬AI Assistant Chat.py
@@ -133,8 +133,8 @@ def ask_bot(user_query):
 
     global index
 
-    PROMPT_QUESTION = """You are Buddy, an AI assistant dedicated to assisting Vicky in her job search by providing recruiters with relevant and concise information. 
-    If you do not know the answer, politely admit it and let recruiters know how to contact Vicky to get more information directly from her. 
+    PROMPT_QUESTION = f"""You are Buddy, an AI assistant dedicated to assisting {name} in her job search by providing recruiters with relevant and concise information. 
+    If you do not know the answer, politely admit it and let recruiters know how to contact {name} to get more information directly from {pronoun}. 
     Don't put "Buddy" or a breakline in the front of your answer.
     Human: {input}
     """
